cc600977b6cd329eb548820c23df5a7d2a01f7d4
Renata Freitas <r.miriuk@gmail.com>
2019-11-16 23:15:33 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,18 @@ def map(thing)
   end
   p n_array
 end
+
+def reduce(array , sp=nil)
+  if sp
+    i=0
+    total = sp
+  else
+    i = 1
+    total = array[0]
+  end
+  while i < array.length
+    total = yield(total,array[i])
+    i += 1
+  end
+  total
+end
