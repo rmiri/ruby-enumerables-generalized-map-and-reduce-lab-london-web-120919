d0070c815d4a55188187b2f68d92aa9a3bbe9f4e
Renata Freitas <r.miriuk@gmail.com>
2019-11-16 20:47:34 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,2 +1,10 @@
 # Your Code Here
-map(array){|n| n * -1}
+def map(thing)
+  n_array = []
+  i = 0
+  while i < thing.length do
+    n_array.push(yield(thing[i]))
+    i += 1
+  end
+  p n_array
+end
